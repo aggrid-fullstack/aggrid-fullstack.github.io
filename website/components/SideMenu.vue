7d8ac333cfe4eee79c63dfab1d9eaf4a99e6fb19
Brandon Govindarajoo <bgovi@umich.edu>
2024-03-26 10:30:48 -0400

    Added documentation to api/index.vue

--- website/components/SideMenu.vue
+++ website/components/SideMenu.vue
@@ -18,6 +18,92 @@
   </template>
   
   <script>
+
+
+
+  export default {
+    // Component logic here
+    // need welcome component and menu bar
+    data() {
+      return {
+        labels: [
+          { group_label: 'Getting Started', 
+              sub_labels: [
+                {route_name: 'OAuth Login',            route_path: '/app_layout/login'},
+                {route_name: 'Architecture',           route_path: '/app_layout/landing_page'},
+                {route_name: 'Environment Variables',  route_path: '/app_layout/grid'},
+                {route_name: 'Docker' ,                route_path: '/app_layout/survey'},
+                {route_name: 'Docker Compose',         route_path: '/app_layout/dynamic_survey'},
+              ]
+            },
+
+          { group_label: 'App Layout', 
+            sub_labels: [
+              {route_name: 'Login',          route_path: '/app_layout/login'},
+              {route_name: 'Landing_Page',   route_path: '/app_layout/landing_page'},
+              {route_name: 'Grid',           route_path: '/app_layout/grid'},
+              {route_name: 'GridMetaData',   route_path: '/app_layout/grid_meta_data'},
+              {route_name: 'Survey',         route_path: '/app_layout/survey'},
+              {route_name: 'Dynamic Survey', route_path: '/app_layout/dynamic_survey'},
+              {route_name: 'SubForms',       route_path: '/app_layout/subforms'},
+              {route_name: 'URL Parameters', route_path: '/app_layout/url_parameters'},
+              {route_name: 'QuickFilter',    route_path: '/app_layout/quick_filter'},
+            ]
+          },
+
+          { group_label: 'API', 
+            sub_labels: [ 
+              {route_name: 'Introduction',  route_path: '/api/'},
+              {route_name: 'Select',        route_path: '/api/select'},
+              {route_name: 'Where',         route_path: '/api/where'},
+              {route_name: 'OrderBy',       route_path: '/api/order_by'},
+              {route_name: 'Pagination',    route_path: '/api/pagination'},
+              {route_name: 'Insert',        route_path: '/api/insert'},
+              {route_name: 'Upsert',        route_path: '/api/upsert'},
+              {route_name: 'Update',        route_path: '/api/update'},
+              {route_name: 'Delete',        route_path: '/api/delete'}, 
+              {route_name: 'DeletedAt',     route_path: '/api/deleted_at'},
+              {route_name: 'Modify',        route_path: '/api/modify'},
+              {route_name: 'Search',        route_path: '/api/search'},
+              {route_name: 'Payload',        route_path:'/api/payload'},
+            ]
+          },
+          { group_label: 'UI_Entry', 
+            sub_labels: [ 
+              {route_name: 'Introduction',    route_path: '/ui/'},
+              {route_name: 'Integer',         route_path: '/ui/select'},
+              {route_name: 'Numeric',         route_path: '/ui/where'},
+              {route_name: 'Text',            route_path: '/ui/order_by'},
+              {route_name: 'DateTime',        route_path: '/ui/pagination'},
+              {route_name: 'Array',           route_path: '/ui/insert'},
+              {route_name: 'JSON',            route_path: '/ui/upsert'},
+              {route_name: 'Actions',         route_path: '/ui/delete'}, 
+              {route_name: 'Boolean',         route_path: '/ui/deleted_at'},
+              {route_name: 'Flag',            route_path: '/ui/modify'},
+              {route_name: 'Formula',         route_path: '/ui/search'},
+              {route_name: 'ReadOnly',        route_path: '/ui/payload'},
+              {route_name: 'Links',           route_path: '/ui/payload'},
+              {route_name: 'Lookup',          route_path: '/ui/payload'},
+              {route_name: 'DropHandle',      route_path: '/ui/drop_handle'},
+              {route_name: 'DropDown',        route_path: '/ui/drop_handle'},
+              {route_name: 'EntryTable',      route_path: '/ui/entry_table'},
+              {route_name: 'Radio',           route_path: '/ui/radio'},
+            ]
+          },
+          { group_label: 'UI_Display', 
+            sub_labels: [ 
+              {route_name: 'Introduction',    route_path: '/ui/'},
+              {route_name: 'ReadOnly',        route_path: '/ui/payload'},
+              {route_name: 'SummaryGrid',     route_path: '/ui/update'},
+              {route_name: 'Plots',           route_path: '/ui/payload'},
+            ]
+          }
+        ]
+        
+      }
+    }
+  }
+
 /*
 Introduction
 Tools And Architecture
@@ -114,51 +200,4 @@ Roles and Permissions
 */
 
 
-  export default {
-    // Component logic here
-    // need welcome component and menu bar
-    data() {
-      return {
-        labels: [
-          { group_label: 'Getting Started', 
-              sub_labels: [
-                {route_name: 'OAuth Login',            route_path: '/app_layout/login'},
-                {route_name: 'Architecture',           route_path: '/app_layout/landing_page'},
-                {route_name: 'Environment Variables',  route_path: '/app_layout/grid'},
-                {route_name: 'Docker' ,                route_path: '/app_layout/survey'},
-                {route_name: 'Docker Compose',         route_path: '/app_layout/dynamic_survey'},
-              ]
-            },
-
-          { group_label: 'App Layout', 
-            sub_labels: [
-              {route_name: 'Login',          route_path: '/app_layout/login'},
-              {route_name: 'Landing_Page',   route_path: '/app_layout/landing_page'},
-              {route_name: 'Grid',           route_path: '/app_layout/grid'},
-              {route_name: 'Survey',         route_path: '/app_layout/survey'},
-              {route_name: 'Dynamic Survey', route_path: '/app_layout/dynamic_survey'},
-            ]
-          },
-
-          { group_label: 'API', 
-            sub_labels: [ 
-              {route_name: 'Introduction',  route_path: '/api/'},
-              {route_name: 'Select',        route_path: '/api/select'},
-              {route_name: 'Insert',        route_path: '/api/insert'},
-              {route_name: 'Upsert',        route_path: '/api/upsert'},
-              {route_name: 'Update',        route_path: '/api/update'},
-              {route_name: 'Delete',        route_path: '/api/delete'}, 
-              {route_name: 'DeletedAt',     route_path: '/api/deleted_at'},
-              {route_name: 'Params',        route_path: '/api/params'},
-            ]
-          },
-
-        ]
-        //ui_labels
-        
-      }
-    }
-
-
-  }
   </script>
--- website/pages/api/index.vue
+++ website/pages/api/index.vue
@@ -12,37 +12,31 @@ data() {
 
         global_directives: {
             title:  "",
+            desc: "primary directives used to describe structure of query and what database objects to use",
             header: ['Directive', 'Description'],
             rows:  [
-                { "directive": "schema",       "description": "", "type": "", "require": ""},
-                { "directive": "tfnc",         "description": "", "type": "", "require": ""},
-                { "directive": "qtype",        "description": "", "type": "", "require": ""},
-                { "directive": "json_blob",    "description": "", "type": "", "require": ""},
-                { "directive": "batch",        "description": "", "type": "", "require": ""},
-                { "directive": "upsert",       "description": "", "type": "", "require": ""},
-                { "directive": "fields/params",    "description": "", "type": "", "require": ""},
-                { "directive": "rls",            "description": "", "type": "", "require": ""},
+                { "directive": "schema", "type": "text", "require": "a-zA-Z_0-9 must start with letter or underscore",       
+                    "description": "name of schema"},
+                { "directive": "tfnc", "type": "", "require": "",         
+                    "description": "name of table/view/function. if fields used creates table operation. if params used creates function operation"},
+                { "directive": "qtype", "type": "", "require": "",        
+                    "description": "how query should be generated options are [select, insert, update, delete, deleted_at, modify, search,upsert]"},
+                { "directive": "json_blob", "type": "", "require": "",    
+                    "description": "if true sends as json dictionary if batch=false otherwise sends as json array"},
+                { "directive": "batch", "type": "", "require": "",
+                    "description": "if true all data is sent as one transaction. default sends each row individualy to server"},
+                { "directive": "upsert", "type": "", "require": "",
+                    "description": "for when qtype is upsert. providers parameters for upsert query"},
+                { "directive": "fields/params", "type": "json", "require": "either fields or params. description of components in fields and table directives below",
+                    "description": `json array describing what data to expect to send and receive to server. if using fields table operations will be created; 
+                        tfnc is then assumed to be table or view. params asume tfnc is a function name. should return a row`},
+                { "directive": "rls", "type": "json object", "require": "optional for select and search routes",
+                    "description": `functionality to add information about user hitting api into the generated query. 
+                        Allows quick implementation of user specific row level security`},
             ]
         },
 
-        /*
-            //field and alias must be alphanumeric and underscore. cant start with a number
-            field: "", //field the actual column name stored in postgres
-            alias: "", //rename of field. this alias value will be used as interface for api column
-            set:   "", //only viable for insert/update/delete or select using params
-            if_null: "", //replaces null value if sent as null
-            default: "", //value injected into data if missing 
-            in:  "", //is the data/filter/order_by in payload used to generate query
-            out: "", //is the field returned back
-            desc: "",//what this fields purposes is
-            required: "", //allowed to be missing. if true and missing send error
-            data_type: "",
-            pk: "" //true/false
-        */
-
-
         // All values are sent and received as strings. Type conversion is done by the browser, data server or end client
-
         /*
             Defaults are values added to the data payload if missing. For fields array If default is not specified nothing is added and 
             the server handles undefined values
@@ -57,46 +51,73 @@ data() {
 
         fields_directives: {
             title:  "",
+            desc:   `
+            fields contains information about each column used such as its name, type and input/output status.
+            fields: [
+                {"field": "column_name",   "data_type": "integer"},
+                {"field": "column_name_2", "data_type": "text"},
+            ] 
+            Fields creates normal sql queries for example:
+
+            select col1, col2 from schema_name.table_name
+            insert into schema_name.tabl_namee (column1, column1) VALUES (value1, value2) RETURNING column1, column2 
+            UPDATE schema_name.table_name SET column_1 = value_1, column_2 = value_2 WHERE id = id_value RETURNING column_1
+            DELETE FROM schema_name.table_name WHERE id = id_value RETURNING x,y,z
+
+            note for nothing to update?
+            `,
             header: ['Directive', 'Description'],
             rows:  [
-                { "directive": "field",       "description": "actual column name stored in postgres", "type": "", "require": ""},
-                { "directive": "alias",       "description": "alias name for api.", "type": "", "require": ""},
-                { "directive": "data_type",   "description": "postgres data type", "type": "", "require": ""},
-                { "directive": "set",         "description": "server injected value for field. default/if_null/required are ignored. key_values described below", "type": "", "require": ""},
-                { "directive": "default",     "description": "", "type": "", "require": ""},
-                { "directive": "if_null",     "description": "", "type": "", "require": ""},
-                { "directive": "required",    "description": "", "type": "", "require": ""},
-                { "directive": "pk",          "description": "", "type": "", "require": ""},
-                { "directive": "in",          "description": "is payload included in query", "type": "", "require": ""},
-                { "directive": "out",         "description": "is field returned", "type": "", "require": ""},
+                { "directive": "field","type": "text", "require": "a-zA-Z_0-9 must start with letter or underscore",
+                    "description": "actual column name stored in postgres"},
+                { "directive": "alias", "type": "text", "require": "a-zA-Z_0-9 must start with letter or underscore",
+                    "description": "alias name for api."},
+                { "directive": "desc",        "description": "meaning of field", "type": "text", "require": ""},
+
+                { "directive": "data_type", "type": "text", "require": "name of field matching postgres type",
+                    "description": "postgres data type. used to convert text to proper value",},
+                { "directive": "set", "type": "text (name of key parameter. value is inject into payload using parameters or for insert/update/delete for fields)", "require": "" ,
+                    "description": "server injected value for field. default/if_null/required are ignored. key_values described below"},
+                { "directive": "default", "type": "text, number or null", "require": "",
+                    "description": "value to inject into payload of key:value pair missing (replaces undefiend values)"},
+                { "directive": "if_null", "type": "boolean", "require": "" ,
+                    "description": "used to replace value of key if its null"},
+                { "directive": "required", "type": "boolean", "require": "", 
+                    "description": "if key is missing api will reject request."},
+                { "directive": "pk", "type": "boolean", "require": "only used in update route. The pk must be present",
+                    "description": "for update route. specifies which keys are part of the where predicate"},
+                { "directive": "in", "type": "boolean", "require": "",
+                    "description": "default true. is payload included in query, filters"},
+                { "directive": "out", "type": "boolean", "require": "",
+                    "description": "default true is field returned"},
             ]
         },
 
         params_in_directives: {
             title:  "",
+            desc: `
             // //order matters
             /*
             //order matters
             params: {
-                input: [
+                in: [
+                    {"field": "input_1", "data_type": "datetime", "set": "now"}
+                    {"field": "input_2": "data_type": "bigint",   "set": "user_id"}
+                ],
+                out: [
                     {"field": "example": "set": "now"}
                     {"field": "example": "set": "user_id"}
-                ]
-
-                output: [
-                    {"field": "example": "set": "now"}
-                    {"field": "example": "set": "user_id"}
-                ]
+                ] 
             }
 
             SELECT ("output_1")::text as "output_alias_1"
             FROM (
-                SELECT "output_1", "output_2" FROM "xschema"."xfunction"(:input_1, :input_2)
+                SELECT "output_1", "output_2" FROM "xschema"."xfunction"(now(), user_id)
                 --where
                 --order by
                 --limit
             ) x
-            */
+           `,
             header: ['Directive', 'Description'], //in or out
             rows:  [
                 { "directive": "field",       "description": "name of the input parameter", "type": "", "require": ""},
@@ -114,17 +135,19 @@ data() {
             title:  "",
             header: ['Directive', 'Description'], //in or out
             rows:  [
-                { "directive": "field",       "description": "", "type": "", "require": ""},
-                { "directive": "alias",       "description": "", "type": "", "require": ""},
-                { "directive": "desc",        "description": "", "type": "", "require": ""},
-                { "directive": "data_type",   "description": "", "type": "", "require": ""},
+                { "directive": "field",       "description": "field/column returned by function", "type": "", "require": ""},
+                { "directive": "alias",       "description": "api alias for output field", "type": "", "require": ""},
+                { "directive": "desc",        "description": "description of output field", "type": "", "require": ""},
+                { "directive": "data_type",   "description": "data type for outputfield", "type": "", "require": ""},
             ]
         },
 
 
         key_values: { //for set
-            title:  `used in set for select, insert,update and delete. Contains information of user calling an api route.
-                now is used for when event occurs. Designed for maintaining deleted_at, updated_at and created_at
+            title: "",
+            desc:  `used in set for qtypes: insert,update and delete when using fields. Always injected for params. 
+                Contains information of user calling an api route.
+                now is used for when event occurs. injects now() function as input Designed for maintaining deleted_at, updated_at and created_at
                 columns if being used All values are sent and received as strings. Type conversion is done by the browser, data server or end client`,
             header: ['Directive', 'Description'],
             rows:  [
